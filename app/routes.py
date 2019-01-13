97633a8c804025dfc5df296b3221d8ef0f2d5490
sanderdan <sander.danielsson@gmail.com>
2019-01-13 17:20:27 +0100

    Added templates.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Sander'}
+    posts = [
+        {
+            'author' : {'username':'Johnny'},
+            'body': 'Nemos pizzeria, such pizza'
+        },
+        {
+            'author': {'username': 'Sandahcity'},
+            'body': 'Wow, Sannegårdens'
+        }
+    ]
+    
+    return render_template('index.html', title='Home', user=user, posts=posts)
